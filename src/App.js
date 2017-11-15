bc78e5d658bdbaaa7a236d143ee77537b5fb8131
EranOV23 <eran.ovadia123@gmail.com>
2017-11-16 01:12:05 +0200

    rewrite app.css + clear console.logs

--- src/App.js
+++ src/App.js
@@ -3,38 +3,50 @@ import logo from './logo.svg';
 import Cocktail from './components/Cocktail';
 import styled from 'styled-components';
 
-import './App.css';
 import CocktailsList from './data/cocktailsList';
 
+const AppWrapper = styled.section`
+  text-align: center;
+`;
 
-const HeadertWrapper = styled.section`
+const HeaderWrapper = styled.section`
   background-color: palevioletred;
   height: 150px;
   padding: 20px;
   color: white;
 `;
 
+const AppLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`;
+
+const AppTitle = styled.h1`
+  font-size: 1.5em;
+`;
+
 const ContentWrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: baseline;
+  height: 500px;
 `;
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HeadertWrapper>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React with Styled-Components & Storybook</h1>
-        </HeadertWrapper>
+      <AppWrapper>
+        <HeaderWrapper>
+          <AppLogo src={logo} alt="logo"></AppLogo>
+          <AppTitle>Welcome to React with Styled-Components & Storybook</AppTitle>
+        </HeaderWrapper>
         <ContentWrapper>
             <Cocktail item={CocktailsList[0]}/>
             <Cocktail recommended item={CocktailsList[1]}/>
             <Cocktail item={CocktailsList[2]}/>
         </ContentWrapper>
-      </div>
+      </AppWrapper>
     );
   }
 }
--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 const Wrapper = styled.section`
     padding: 4em;
     background: ${props => props.selected ? 'salmon' : 'gold'};
     color: white;
     font-size: 20px;
+    min-height: 700px;
 `;
 
 const Title = styled.h1`
@@ -21,13 +21,18 @@ const Img = styled.img`
 `;
 
 const Button = styled.button`
-background: ${props => props.recommended ? 'palevioletred' : 'white'};
-color: ${props => props.recommended ? 'white' : 'palevioletred'};
-font-size: 1em;
-margin: 1em;
-padding: 0.25em 1em;
-border: 2px solid palevioletred;
-border-radius: 3px;
+    background: ${props => props.recommended ? 'palevioletred' : 'white'};
+    color: ${props => props.recommended ? 'white' : 'palevioletred'};
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+`;
+
+const Bold = styled.span`
+    font-weight: bold;
+    color: salmon
 `;
 
 export default class Cocktail extends React.Component {
@@ -39,14 +44,13 @@ export default class Cocktail extends React.Component {
     }
 
     render() {
-        console.log(this.props)        
         return (
             <Wrapper selected={this.state.selected} onClick={()=>this.setState({selected: !this.state.selected})}>
                 <Img src={this.props.item.img} alt="cocktail"></Img> 
                 <Title>{this.props.item.title}</Title>
-                <p><strong>By</strong> {this.props.item.author}</p>
+                <p><Bold>By</Bold> {this.props.item.author}</p>
                 <p>{this.props.item.description}</p>
-                <p><strong>Tags: </strong>{this.props.item.tags.map((tag, i) => {return <span className="tag" key={i}> {tag} </span>})}</p>
+                <p><Bold>Tags:</Bold> {this.props.item.tags.map((tag, i) => {return <span className="tag" key={i}> {tag} </span>})}</p>
                 <Button recommended={this.props.recommended}>Read more</Button>
             </Wrapper>
         )
